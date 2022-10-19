0186b3f501a723feaa80f15aedba1a6d36249c90
rc-ops <rodrigocarvalhocontato@protonmail.com>
2022-10-19 20:01:08 -0300

    Add project files

---
+++ src/main/java/com/example/sistemacondominio/SistemaCondominioApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemacondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCondominioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaCondominioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sistemacondominio/SistemaCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemacondominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCondominioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
